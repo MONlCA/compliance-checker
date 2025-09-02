e9f74e2bf0c04e70a5655a05326fba808f4edc4c
MONlCA <m.pras@mail.com>
2025-09-01 19:13:48 -0700

    Fix relative import for Streamlit Cloud

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from compliance_logic import check_compliance
 
-from .utils import extract_text_from_image, extract_text_from_url
+from utils import extract_text_from_image, extract_text_from_url
 
 st.set_page_config(page_title="A2P/TFV Compliance Assistance", layout="wide")
 
